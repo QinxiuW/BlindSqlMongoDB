4493f62dc71582e21fd3dcc6999fb7719921052e
Wqinx <wqinxiu@gmail.com>
2022-12-25 12:54:16 +0100

    updated

---
+++ src/main/java/com/example/blindsqlmongodb/BlindSqlMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.example.blindsqlmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlindSqlMongoDbApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BlindSqlMongoDbApplication.class, args);
+  }
+
+}
